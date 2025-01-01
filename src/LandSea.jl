81099d05661604f0f6d69678a075c58167bee8ab
Nathanael Wong <natgeo.wong@outlook.com>
2025-01-01 19:02:47 +0800

    LandSea is now a lightweight package without ETOPO downloads

--- src/LandSea.jl
+++ src/LandSea.jl
@@ -1,61 +1,59 @@
 module LandSea
 
 using Dates
-using ImageFiltering
+# using ImageFiltering
 using Logging
-using NCDatasets
 
-import Base: show, read, in
-
-using Reexport
-@reexport using GeoRegions
+import Base: show
 
 ## Exporting the following functions:
 export
         LandSeaData, LandSeaTopo, LandSeaFlat,
-        getLandSea, smooth, smooth!, smoothlsm
+        getLandSea
+        #smooth, smooth!, smoothlsm
 
 ## Abstract Types
 """
     LandSeaData
 
-Abstract supertype for LandSea Datasets, with the following subtypes:
-
-    LandSeaTopo <: LandSeaData
-    LandSeaFlat <: LandSeaData
-
-Both `LandSeaTopo` and `LandSeaFlat` types contain the following fields:
+Abstract supertype for LandSea Datasets. All `LandSeaData` types contain the following fields:
 * `lon` - Vector containing the longitude points for the Land-Sea Dataset
 * `lat` - Vector containing the latitude points for the Land-Sea Dataset
-* `lsm` - Array containing data regarding the Land-Sea Mask.  1 is Land, 0 is Ocean, NaN is outside the bounds of the GeoRegion
-* `mask` - Mask determining if point is within the GeoRegion or not. 1 is `true`, 0 is `false`.
-
-A `LandSeaTopo` type will also contain the following field:
-* `z` - Array containing data regarding the Orographic Height in meters.  NaN is outside the bounds of the GeoRegion
+* `lsm` - Array containing data regarding the Land-Sea Mask. 1 is Land, 0 is Ocean, NaN is outside the bounds of the GeoRegion
 """
 abstract type LandSeaData end
 
-abstract type LandSeaTopo <: LandSeaData end
+"""
+    LandSeaTopo <: LandSeaData
 
-abstract type LandSeaFlat <: LandSeaData end
+Text
+
+A `LandSeaTopo` type will also contain the following field:
+* `z` - Array containing data regarding the Orographic Height in meters. NaN is outside the bounds of the GeoRegion
+"""
+struct LandSeaTopo{FT1<:Real,FT2<:Real} <: LandSeaData
+    lon :: Vector{FT1}
+    lat :: Vector{FT1}
+    lsm :: Array{FT2,2}
+    z   :: Array{FT2,2}
+end
 
 """
-    LandSea.ETOPOLandSea <: LandSeaTopo
+    LandSeaFlat <: LandSeaData
 
-Object containing information on the ETOPO 2022 Land Sea mask for a GeoRegion.
+Text
 """
-struct ETOPOLandSea <: LandSeaTopo
-    lon  :: Vector{Float32}
-    lat  :: Vector{Float32}
-    lsm  :: Array{Float32,2}
-    z    :: Array{Float32,2}
-    mask :: Array{Int16,2}
+struct LandSeaFlat{FT1<:Real,FT2<:Real} <: LandSeaData
+    lon :: Vector{FT1}
+    lat :: Vector{FT1}
+    lsm :: Array{FT2,2}
 end
 
 modulelog() = "$(now()) - LandSea.jl"
 
-include("download.jl")
-include("smooth.jl")
-include("backend.jl")
+getLandSea() = "$(modulelog()) - There is a getLandSea() dummy-function in LandSea.jl, so that it can be extended by other packages"
+
+# include("smooth.jl")
+include("show.jl")
 
 end
